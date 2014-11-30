2fc4c9c3dc5dc9f68e0cbb80a6c90b46df9bd236
Brecht Gosselé <brecht.gossele@gmail.com>
2014-11-30 19:24:54 +0100

    straight tail

--- tron.elm
+++ tron.elm
@@ -15,25 +15,26 @@ height = 600
 
 playerW = 64
 playerH = 16
+tailL = toFloat 256
+tailOffset = toFloat 4
 
 data KeybInput = KeybInput (Int, Int) (Int, Int) Bool
+data Input = Input KeybInput Time
 
-heartbeat = lift (Debug.watch "heartbeat") (fps 24)
+heartbeat = lift (Debug.watch "heartbeat") (fps 120)
 
 keybInput : Signal KeybInput
 keybInput =
-    let realInput = lift3 KeybInput (lift (\{x,y} -> (x,y)) Keyboard.arrows) (lift (\{x,y} -> (x,y)) Keyboard.wasd) Keyboard.space
+    let realInput = lift3 KeybInput (lift (\{x,y} -> (x,y)) Keyboard.wasd) (lift (\{x,y} -> (x,y)) Keyboard.arrows) Keyboard.space
     in lift (Debug.watch "keybInput") realInput
 
-data Input = Input KeybInput Time
-
 timedInput : Signal Input
-timedInput = lift (Debug.watch "timed input") (lift2 Input keybInput heartbeat)
+timedInput = lift2 Input keybInput heartbeat
 
 initialGameState : GameState
 initialGameState =
-    let bike1 = BikeState (-(width/2)+50, 0.0) E 2 []
-        bike2 = BikeState (width/2-50, 0.0) W 2 []
+    let bike1 = BikeState (-(width/2)+50, 0.0) E 0.75 []
+        bike2 = BikeState (width/2-50, 0.0) W 0.75 []
     in Playing bike1 bike2 True
 
 showGameState : GameState -> Element
@@ -42,8 +43,13 @@ showGameState gs =
         elements = case gs of
                      (Ended _)-> [toForm (centered (Text.color white (toText "Press space to start playing")))]
                      (Playing (BikeState pos o _ tail) (BikeState pos2 o2 _ tail2) _) -> 
-                         [showPlayer' Color.red pos o,
-                          showPlayer' Color.blue pos2 o2]
+                         let tailstart = pointInDirection pos o -tailOffset
+                             tailstart2 = pointInDirection pos2 o2 -tailOffset
+                         in
+                             [showPlayer' Color.red pos o,
+                              showTail Color.red (tailstart :: (generateTail tailstart tail tailL o)),
+                              showPlayer' Color.blue pos2 o2,
+                              showTail Color.blue (tailstart2 :: (generateTail tailstart2 tail2 tailL o2))]
     in collage width height forms
 
 step : Input -> GameState -> GameState
@@ -53,23 +59,29 @@ step (Input (KeybInput arrows wasd space) _) gs =
         Playing (BikeState pos1 o1 v1 tail1) (BikeState pos2 o2 v2 tail2) b -> 
             let new_o1 = getNewOrientation arrows o1
                 new_o2 = getNewOrientation wasd o2
-                new_pos1 = updatePosition pos1 o1 v1
-                new_pos2 = updatePosition pos2 o2 v2
+                new_pos1 = pointInDirection pos1 o1 v1
+                new_pos2 = pointInDirection pos2 o2 v2
             in 
-                if space /= b && space then Ended space else Playing (BikeState new_pos1 new_o1 v1 tail1) (BikeState new_pos2 new_o2 v2 tail2) space
+                if space /= b && space then initialGameState else Playing (BikeState new_pos1 new_o1 v1 tail1) (BikeState new_pos2 new_o2 v2 tail2) space
 
 getNewOrientation : (Int,Int) -> Orientation -> Orientation
 getNewOrientation (x,y) o = if x == 1 then E else (if x == -1 then W else (if y == 1 then N else (if y == -1 then S else o)))
 
-updatePosition : Pos -> Orientation -> Float -> Pos
-updatePosition (x,y) o v =
+pointInDirection : Pos -> Orientation -> Float -> Pos
+pointInDirection (x,y) o distance =
     case o of
-        N -> (x, y + v)
-        E -> (x + v, y)
-        S -> (x, y - v)
-        W -> (x - v, y)
+        N -> (x, y + distance)
+        E -> (x + distance, y)
+        S -> (x, y - distance)
+        W -> (x - distance, y)
 
-gameState = lift (Debug.watch "Timed game state") (foldp step (Ended False) timedInput)
+generateTail : Pos -> [Pos] -> Float -> Orientation -> [Pos]
+generateTail (x,y) corners length or =
+    case corners of
+        [] -> [(x,y), pointInDirection (x,y) or -length]
+        c::cs -> c::cs 
+
+gameState = lift (Debug.watch "Game state") (foldp step (Ended False) timedInput)
 
 main = lift showGameState gameState
 
