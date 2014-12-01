40fbaa087f28634af0dfdb0ce99dfb2a1cebdc6a
Brecht Gosselé <brecht.gossele@gmail.com>
2014-12-01 12:47:35 +0100

    collision detection

--- tron.elm
+++ tron.elm
@@ -1,3 +1,5 @@
+module Tron where
+
 import Color
 import Text
 import Keyboard
@@ -7,7 +9,7 @@ import Basics
 type Pos = (Float, Float)
 type Tail = [Pos]
 
-data GameState = Ended Bool | Playing BikeState BikeState Bool
+data GameState = Ended String Bool | Playing BikeState BikeState Bool
 data BikeState = BikeState Pos Orientation Float Tail
 
 -- default values
@@ -44,18 +46,18 @@ showGameState : GameState -> Element
 showGameState gs = 
     let forms = [filled black (rect width height)] ++ elements
         elements = case gs of
-                     (Ended _)-> [toForm (centered (Text.color white (toText "Press space to start playing")))]
+                     (Ended m _)-> [toForm (centered (Text.color white (toText m)))]
                      (Playing (BikeState pos1 o1 _ tail1) (BikeState pos2 o2 _ tail2) _) ->
                          [showPlayer' Color.red pos1 o1,
-                          showTail Color.red tail1,
+                          showLine Color.red tail1,
                           showPlayer' Color.blue pos2 o2,
-                          showTail Color.blue tail2]
+                          showLine Color.blue tail2]
     in collage width height forms
 
 step : Input -> GameState -> GameState
 step (Input (KeybInput arrows wasd space) _) gs =
     case gs of
-        Ended b -> if space /= b && space then initialGameState else Ended space
+        Ended m b -> if space /= b && space then initialGameState else Ended m space
         Playing (BikeState pos1 o1 v1 tail1) (BikeState pos2 o2 v2 tail2) b -> 
             let new_o1 = getNewOrientation arrows o1
                 new_o2 = getNewOrientation wasd o2
@@ -65,8 +67,15 @@ step (Input (KeybInput arrows wasd space) _) gs =
                 tailstart2 = pointInDirection new_pos2 new_o2 -tailOffset
                 new_tail1 = tailstart1 :: (generateTail tailstart1 tail1 tailL)
                 new_tail2 = tailstart2 :: (generateTail tailstart2 tail2 tailL)
-            in 
-                if space /= b && space then initialGameState else Playing (BikeState new_pos1 new_o1 v1 new_tail1) (BikeState new_pos2 new_o2 v2 new_tail2) space
+                outOfBounds1 = outOfBounds (corners new_pos1 new_o1)
+                collision1 = (collideWithTail new_pos1 new_o1 new_tail1) || (collideWithTail new_pos1 new_o1 new_tail2)
+                outOfBounds2 = outOfBounds (corners new_pos2 new_o2)
+                collision2 = (collideWithTail new_pos2 new_o2 new_tail1) || (collideWithTail new_pos2 new_o2 new_tail2)
+            in
+                if outOfBounds1 || collision1 then Ended "Player 2 wins!" space else
+                    if outOfBounds2 || collision2 then Ended "Player 1 wins!" space else
+                        if space /= b && space then initialGameState else
+                            Playing (BikeState new_pos1 new_o1 v1 new_tail1) (BikeState new_pos2 new_o2 v2 new_tail2) space
 
 getNewOrientation : (Int,Int) -> Orientation -> Orientation
 getNewOrientation (x,y) o = if x == 1 then E else (if x == -1 then W else (if y == 1 then N else (if y == -1 then S else o)))
@@ -80,14 +89,14 @@ pointInDirection (x,y) o distance =
         W -> (x - distance, y)
 
 generateTail : Pos -> [Pos] -> Float -> [Pos]
-generateTail (x,y) corners length =
+generateTail (x,y) points length =
     if length == 0 then [] else
-        case corners of
+        case points of
             [] -> []
-            c::cs ->
-                let d = dist (x,y) c
+            p::ps ->
+                let d = dist (x,y) p
                 in
-                    if d <= length then c :: (generateTail c cs (length - d)) else [findPoint (x,y) c length]
+                    if d <= length then p :: (generateTail p ps (length - d)) else [findPoint (x,y) p length]
 
 sq x = x * x
 sign x = if x > 0 then 1 else (if x == 0 then 0 else -1)
@@ -98,7 +107,31 @@ findPoint (x1, y1) (x2, y2) dist = (x1 + (sign (x2-x1))*dist, y1 + (sign (y2-y1)
 dist : Pos -> Pos -> Float
 dist (x1,y1) (x2,y2) = sqrt(sq (x1-x2) + sq (y1-y2))
 
-gameState = lift (Debug.watch "Game state") (foldp step (Ended False) timedInput)
+corners : Pos -> Orientation -> [Pos]
+corners (x,y) o =
+    let (xt, yt) = pointInDirection (x,y) o playerW
+    in
+        case o of
+            E -> [(x,y+playerH/2),(x,y-playerH/2),(xt,y+playerH/2),(xt,y-playerH/2)]
+            W -> [(x,y+playerH/2),(x,y-playerH/2),(xt,y+playerH/2),(xt,y-playerH/2)]
+            _ -> [(x+playerH/2,y),(x-playerH/2,y),(x+playerH/2,yt),(x-playerH/2,yt)]
+
+outOfBounds : [Pos] -> Bool
+outOfBounds l = foldr (\(x,y) b -> b || ((abs x > width/2) || (abs y > height/2))) False l
+
+pointInBox : Float -> Float -> Float -> Float -> Pos -> Bool
+pointInBox x1 x2 y1 y2 (px, py) = (sign(x1-px) /= sign (x2-px)) && (sign(y1-py) /= sign(y2-py))
+
+collideWithTail (x,y) o =
+        let f = case o of
+            N -> pointInBox (x-playerH/2) (x+playerH/2) y (y+playerW)
+            E -> pointInBox x (x+playerW) (y-playerH/2) (y+playerH/2)
+            S -> pointInBox (x-playerH/2) (x+playerH/2) y (y-playerW)
+            W -> pointInBox x (x-playerW) (y-playerH/2) (y+playerH/2)
+        in
+            foldr (\p b -> b || f p) False
+
+gameState = lift (Debug.watch "Game state") (foldp step (Ended  "Press space to start playing" False) timedInput)
 
 main = lift showGameState gameState
 
@@ -121,9 +154,9 @@ showPlayer' color (x, y) o =
 
 tailWidth = 2
 
--- example use: showTail Color.lightBlue [(10, 10), (20, 10)]
-showTail : Color -> [(Float, Float)] -> Form
-showTail color positions = 
+-- example use: showLine Color.lightBlue [(10, 10), (20, 10)]
+showLine : Color -> [(Float, Float)] -> Form
+showLine color positions = 
     traced { defaultLine |
         width <- tailWidth
         , color <- color
