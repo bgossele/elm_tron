98bc7f53a2debb2f97b7cd61a817234dcceafd45
Brecht Gosselé <brecht.gossele@gmail.com>
2014-12-04 11:48:46 +0100

    growing tail

--- tron.elm
+++ tron.elm
@@ -10,7 +10,7 @@ type Pos = (Float, Float)
 type Tail = [Pos]
 
 data GameState = Ended String Bool | Playing BikeState BikeState Bool
-data BikeState = BikeState Pos Orientation Float Tail
+data BikeState = BikeState Pos Orientation Float Float Tail
 
 -- default values
 width = 800
@@ -18,13 +18,15 @@ height = 600
 
 playerW = 64
 playerH = 16
+frameRate = 100
+tailIncrement = 1/frameRate
 tailL = toFloat 256
 tailOffset = toFloat 4
 
 data KeybInput = KeybInput (Int, Int) (Int, Int) Bool
 data Input = Input KeybInput Time
 
-heartbeat = lift (Debug.watch "heartbeat") (fps 100.0)
+heartbeat = lift (Debug.watch "heartbeat") (fps frameRate)
 
 keybInput : Signal KeybInput
 keybInput =
@@ -38,8 +40,8 @@ initialGameState : GameState
 initialGameState =
     let pos1 = (-width/2+50, 0.0)
         pos2 = (width/2-50, 0.0)
-        bike1 = BikeState pos1 E 0.75 [pointInDirection pos1 E -tailOffset]
-        bike2 = BikeState pos2 W 0.75 [pointInDirection pos2 W -tailOffset]
+        bike1 = BikeState pos1 E 0.75 tailL [pointInDirection pos1 E -tailOffset]
+        bike2 = BikeState pos2 W 0.75 tailL [pointInDirection pos2 W -tailOffset]
     in Playing bike1 bike2 True
 
 showGameState : GameState -> Element
@@ -47,7 +49,7 @@ showGameState gs =
     let forms = [filled black (rect width height)] ++ elements
         elements = case gs of
                      (Ended m _)-> [toForm (centered (Text.color (rgb 0 204 0) (toText (m ++ "\n\n\n========> Press space to start <========"))))]
-                     (Playing (BikeState pos1 o1 _ tail1) (BikeState pos2 o2 _ tail2) _) ->
+                     (Playing (BikeState pos1 o1 _ _ tail1) (BikeState pos2 o2 _ _ tail2) _) ->
                          [showPlayer' Color.red pos1 o1,
                           showLine Color.red tail1,
                           showPlayer' Color.darkBlue pos2 o2,
@@ -58,15 +60,17 @@ step : Input -> GameState -> GameState
 step (Input (KeybInput arrows wasd space) _) gs =
     case gs of
         Ended m b -> if space /= b && space then initialGameState else Ended m space
-        Playing (BikeState pos1 o1 v1 tail1) (BikeState pos2 o2 v2 tail2) b -> 
+        Playing (BikeState pos1 o1 v1 tailL1 tail1) (BikeState pos2 o2 v2 tailL2 tail2) b -> 
             let new_o1 = getNewOrientation arrows o1
                 new_o2 = getNewOrientation wasd o2
                 new_pos1 = pointInDirection pos1 new_o1 (if o1 == new_o1 then v1 else tailOffset)
                 new_pos2 = pointInDirection pos2 new_o2 (if o2 == new_o2 then v2 else tailOffset)
                 tailstart1 = pointInDirection new_pos1 new_o1 -tailOffset
                 tailstart2 = pointInDirection new_pos2 new_o2 -tailOffset
-                new_tail1 = tailstart1 :: (generateTail tailstart1 tail1 tailL)
-                new_tail2 = tailstart2 :: (generateTail tailstart2 tail2 tailL)
+                new_tail1 = tailstart1 :: (generateTail tailstart1 tail1 tailL1)
+                new_tail2 = tailstart2 :: (generateTail tailstart2 tail2 tailL2)
+                new_tailL1 = Debug.watch "tailL" (tailL1 + tailIncrement)
+                new_tailL2 = tailL2 + tailIncrement
                 outOfBounds1 = outOfBounds (corners new_pos1 new_o1)
                 collision1 = (collideWithTail new_pos1 new_o1 new_tail1) || (collideWithTail new_pos1 new_o1 new_tail2)
                 outOfBounds2 = outOfBounds (corners new_pos2 new_o2)
@@ -75,7 +79,7 @@ step (Input (KeybInput arrows wasd space) _) gs =
                 if outOfBounds1 || collision1 then Ended "Player 2 wins!" space else
                     if outOfBounds2 || collision2 then Ended "Player 1 wins!" space else
                         if space /= b && space then initialGameState else
-                            Playing (BikeState new_pos1 new_o1 v1 new_tail1) (BikeState new_pos2 new_o2 v2 new_tail2) space
+                            Playing (BikeState new_pos1 new_o1 v1 new_tailL1 new_tail1) (BikeState new_pos2 new_o2 v2 new_tailL2 new_tail2) space
 
 getNewOrientation : (Int,Int) -> Orientation -> Orientation
 getNewOrientation (x,y) o = if x == 1 then E else (if x == -1 then W else (if y == 1 then N else (if y == -1 then S else o)))
@@ -131,7 +135,9 @@ collideWithTail (x,y) o =
         in
             foldr (\p b -> b || f p) False
 
-gameState = lift (Debug.watch "Game state") (foldp step (Ended banner False) input)
+--gameState = lift (Debug.watch "Game state") (foldp step (Ended banner False) input)
+gameState = foldp step (Ended banner False) input
+
 
 main = lift showGameState gameState
 
